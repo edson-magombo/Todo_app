d4fc8623a714cb304c1f1db0c4f4f778fc26c8c3
edson-magombo <bed-com-33-18@cc.ac.mw>
2021-08-22 15:17:49 +0200

    process

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 import "./Header.css";
 import { useStateValue } from './StateProvider';
 import { auth } from '../firebase';
-import reducer from './reducer';
 
 function Header() {
   const [{user}]= useStateValue();
@@ -22,7 +21,7 @@ function Header() {
       </div>
       <div className = "menu">
         <Link className = "link" to = "/">HOME</Link>
-        <Link className = "link" to = "/">INQUIRES</Link>
+        <Link className = "link" to = "/Inquiry">INQUIRES</Link>
         <Link className = "link" to = "/">ABOUT US</Link>
       </div>
       </div>
@@ -44,13 +43,4 @@ function Header() {
 export default Header;
 
 
-
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-// <script src="/__/firebase/8.8.0/firebase-app.js"></script>
-
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#available-libraries -->
-// <script src="/__/firebase/8.8.0/firebase-analytics.js"></script>
-
-// <!-- Initialize Firebase -->
-// <script src="/__/firebase/init.js"></script>
+ 
--- src/component/Inquiry.js
+++ src/component/Inquiry.js
@@ -4,6 +4,7 @@ import Avatar  from "@material-ui/core/Avatar";
 function Inquiry({displayName, timestamp, imageURL, message}) {
    
     return (
+        <div>
         <div className = "inquiry">
              {/* image */}
              <div className = "inquiry_header">
@@ -21,6 +22,10 @@ function Inquiry({displayName, timestamp, imageURL, message}) {
            
             {/* inquiry */}
             <h3 className = "message">{message}</h3>
+        </div>
+        <div> 
+            <h1>hello inquiry</h1>
+        </div>
         </div>
     )
 }
